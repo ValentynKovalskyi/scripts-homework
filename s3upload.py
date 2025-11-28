d1b72d17d3c901f5259862f0d166bd919d422a51
Valentyn Kovalskyi <kovalsky.valentine@gmail.com>
2025-11-28 16:19:24 +0200

    Fix logic, delete redundant print

--- s3upload.py
+++ s3upload.py
@@ -2,15 +2,14 @@ import boto3
 import sys
 from pathlib import Path
 
-print(len(sys.argv))
 if len(sys.argv) != 2:
 	print("Specify path to file as argument 1")
 	sys.exit(1)
 
 uploadFilePath = Path(sys.argv[1])
 
-if not uploadFilePath.exists():
-	print("Specified path is not existing directory or file")
+if not uploadFilePath.is_file():
+	print("Specified path is not existing file")
 	sys.exit(1)
  
 s3 = boto3.client('s3')
